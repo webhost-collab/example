4ebcc4f7ae87ec0b99f2d5ac327733618bfdbf9a
webhost-collab <parjapathashokkumar135@gmail.com>
2025-03-18 15:07:40 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-# simple_script.py
-import datetime
-
-def main():
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f"Hello, Ashok! The current time is {current_time}.")
-
-if __name__ == "__main__":
-    main()
